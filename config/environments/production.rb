87c196b47f761dce3bb9a86ceabe113f205fa74b
Yuri Correa <yuri.correa@jumseller.com>
2023-09-20 15:44:41 -0300

    #3 add hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.hosts << "yuricorrea-dev.sa-east-1.elasticbeanstalk.com"
 end
