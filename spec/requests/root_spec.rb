c7191d4dda7d780f6e57ca4334ca0299b207da91
Yuri Correa <yuri.correa@jumseller.com>
2023-09-20 17:55:55 -0300

    #4 test coverage

--- spec/requests/root_spec.rb
+++ spec/requests/root_spec.rb
@@ -10,11 +10,6 @@ RSpec.describe 'Roots' do
       get root_path
     end
 
-    it 'returns a JSON response with the text "Hello"', :aggregate_failures do
-      skip 'testing github'
-      expect(response).to have_http_status(:ok)
-      expect(response.content_type).to eq('application/json; charset=utf-8')
-      expect(parsed_response).to eq({ 'text' => 'Hello' })
-    end
+   
   end
 end
