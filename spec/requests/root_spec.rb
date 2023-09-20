e5f6d3670948a5080977ddc2ffd1d3dae657bac2
Yuri Correa <yuri.correa@jumseller.com>
2023-09-20 17:55:58 -0300

    #4 test coverage

--- spec/requests/root_spec.rb
+++ spec/requests/root_spec.rb
@@ -1,15 +1,3 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-
-RSpec.describe 'Roots' do
-  describe 'GET /index' do
-    let(:parsed_response) { response.parsed_body }
-
-    before do
-      get root_path
-    end
-
-   
-  end
-end
