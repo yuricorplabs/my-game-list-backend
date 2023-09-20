b28f7545eb886a3d67af24b0018bb5f15567ca1f
Yuri Correa <yuri.correa@jumseller.com>
2023-09-20 17:46:44 -0300

    #4 test coverage

--- spec/requests/root_spec.rb
+++ spec/requests/root_spec.rb
@@ -4,11 +4,15 @@ require 'rails_helper'
 
 RSpec.describe 'Roots' do
   describe 'GET /index' do
-    it 'returns a JSON response with the text "Hello"', :aggregate_failures do
+    let(:parsed_response) { response.parsed_body }
+    
+    before do
       get root_path
+    end
+    it 'returns a JSON response with the text "Hello"', :aggregate_failures do
+      pending 'testing github'
       expect(response).to have_http_status(:ok)
       expect(response.content_type).to eq('application/json; charset=utf-8')
-      parsed_response = response.parsed_body
       expect(parsed_response).to eq({ 'text' => 'Hello' })
     end
   end
