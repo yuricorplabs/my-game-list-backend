3e547170a2b346c33e9ee051911d7027a529c8a3
Yuri Correa <yuri.correa@jumseller.com>
2023-10-03 11:24:50 -0300

    add game to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first
+User.destroy_all
+Publisher.destroy_all
 
 FactoryBot.create(:user)
+FactoryBot.create_list(:game, 10)
