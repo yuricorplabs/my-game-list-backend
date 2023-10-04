71c30f61cf1e8624bd9195539713502a811e6297
Yuri Correa <yuri.correa@jumseller.com>
2023-10-03 22:36:48 -0300

    add search endpoint #34

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,11 @@ Rails.application.routes.draw do
                      }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :games
+  resources :games do
+    collection do
+      get 'search/:term', to: 'games#search', as: :search
+    end
+  end
   # Defines the root path route ("/")
   root 'root#index'
 end
